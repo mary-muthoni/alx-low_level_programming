e24cb5c0af14d20d9aae5570a9692a0fe877739b
mary-muthoni <kamaum772@gmail.com>
2022-09-13 14:13:09 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -> prints the lowercase alphabets
+ */
+void print_alphabet_x10(void)
+
+{
+	int j;
+	int x;
+
+	for (x = 0; x < 10; x++)
+
+	{
+		_putchar(j);
+
+	}
+	_ptchar('\n');
+
+}
