14971239b722ff032e8b3691077fd258091be668
mary-muthoni <kamaum772@gmail.com>
2022-09-13 13:59:23 -0700

    task 0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- 
  * print_alphabet-> prints the lowercase alphabets
  *
  */
@@ -10,7 +9,7 @@ void print_alphabet(void)
 {
 	int j;
 
-	for (j = 'a'; j <='z'; j++)
+	for (j = 'a'; j <= 'z'; j++)
 
 	{
 		_putchar(j);
