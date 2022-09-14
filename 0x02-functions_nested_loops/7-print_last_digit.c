32ecf33a1e74fe566ed7e42622414129ed43227a
mary-muthoni <kamaum772@gmail.com>
2022-09-14 12:02:37 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_last_digit - prints the last digit of a numer.
+ * print_last_digit - prints the last digit of a number.
  * @n: digit to find the last place of.
  * Return: The last digit.
  */
