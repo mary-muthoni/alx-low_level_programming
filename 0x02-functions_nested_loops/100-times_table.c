ffeb85abf1f42cb94af772207bcd6905388eef85
mary-muthoni <kamaum772@gmail.com>
2022-09-14 11:57:13 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,6 +26,12 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar((res % 10) + '0');
 				}
+				else if (res >= 10 && res < 100)
+				{
+					_putchar(' ');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
 				else if (res >= 100 && j != 0)
 				{
 					_putchar((res / 100) + '0');
