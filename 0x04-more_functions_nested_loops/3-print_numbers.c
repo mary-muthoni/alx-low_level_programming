fe0dee06af5377d51e0622440bbaaff0323fa8ad
mary-muthoni <kamaum772@gmail.com>
2022-09-15 11:19:12 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print numbers - print the numbers from 0 up to 9
+ * print numbers - print the numbers since 0 up to 9
  *
- * Return: The numbers from 0 up to 9
+ * Return: The numbers since 0 up to 9
  */
 void print_numbers(void)
 {
