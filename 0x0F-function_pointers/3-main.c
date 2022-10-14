bc8f49ccf7770a5d7f2a1c12f11576ed8e5a0d36
mary-muthoni <kamaum772@gmail.com>
2022-10-14 08:23:13 -0700

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,33 +1,48 @@
-#include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/**
- * main - program to perform simple operations
- * @argc: argument count
- * @argv: array of arguments
- *
- * Return: exists 0 normally, 98 on argc error, 99 on illegal operator
+/*
+ * File: 3-main.c
+ * Auth: Brennan D Baraban
  */
 
-int main(int argc, char *argv[])
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int (*func)(int, int);
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
-		print("Error\n");
+		printf("Error\n");
 		exit(98);
 	}
 
-	func = get_op_func(argv[2]);
-	if (func == NULL)
-		{
-	print("Error\n");
-	exit(99)'
-		}
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
-	print("%d\n", func(atoi(argv[1]), atoi(argv[3]));
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && num2 == 0) ||
+			(*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
