78de3d6805b2ceef84114304826ba42dab501dd5
mary-muthoni <kamaum772@gmail.com>
2022-10-26 11:37:41 -0700

    task 13

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * find_listint_loop - define function
+ * @head: only declare a maximum of two variables in your function
+ * Return: what does it return?
+ */
+listint_t *find_listint_loop(listint_t *head);
+{
+	head++;
+	return (0);
+}
